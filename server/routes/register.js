e228e11bfde9cb84bb8fd1feb8a463fb7a29bc40
John Watson <johnwatson484@hotmail.com>
2019-03-31 21:04:18 +0100

    Register route added

---
+++ server/routes/register.js
@@ -0,0 +1,19 @@
+const schema = require('../schema/user')
+
+module.exports = {
+  method: 'POST',
+  path: '/register',
+  options: {
+    validate: { payload: schema,
+      failAction: async (request, h, error) => {
+        console.log(`rejected payload ${payload}`)
+        return h.response().code(400)
+      }
+    },
+    handler: async (request, h) => {
+      console.log('new user received')
+      
+      return h.response().code(200)
+    }
+  }
+}
