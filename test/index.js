16d0f4a5971e1b92b6ae769f41a3747a6ce3b26b
John Watson <johnwatson484@hotmail.com>
2019-04-14 15:52:43 +0100

    Unit tests updated

--- test/index.js
+++ test/index.js
@@ -1,35 +1,29 @@
 const Lab = require('lab')
 const Code = require('code')
 const lab = exports.lab = Lab.script()
-const createServer = require('../server')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire').noCallThru()
 
 lab.experiment('API test', () => {
-  let server
+  let sandbox
+  let service
+
+  const userRepositoryMock = require('./repository/user-repository.mock')
 
   // Create server before each test
   lab.before(async () => {
-    server = await createServer()
+    sandbox = sinon.createSandbox()
+    service = proxyquire('../server/services/user-service', {
+      '../repository/user-repository': userRepositoryMock
+    })
   })
 
-  lab.test('GET / route works', async () => {
-    const options = {
-      method: 'GET',
-      url: '/'
-    }
-
-    const response = await server.inject(options)
-    Code.expect(response.statusCode).to.equal(200)
-    Code.expect(response.result).to.equal({ hello: 'world' })
+  lab.afterEach(async () => {
+    sandbox.restore()
   })
 
-  lab.test('GET /about route works', async () => {
-    const options = {
-      method: 'GET',
-      url: '/about'
-    }
-
-    const response = await server.inject(options)
-    Code.expect(response.statusCode).to.equal(200)
-    Code.expect(response.result).to.equal({ ok: 200 })
+  lab.test('user service creates', async () => {
+    const response = await service.create({ email: 'test2@email.com' })
+    Code.expect(response).to.equal(true)
   })
 })
---
+++ test/repository/user-repository.mock.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  getByEmail: async function (email) {
+    return undefined
+  },
+  create: async function (user) {
+    return true
+  }
+}
