9ee12ea2498b6ada44f047fede742e48a9476374
markysoft <markharrop@yahoo.com>
2019-07-04 08:55:11 +0100

    replace double quotes with single quotes

--- server/config/config.js
+++ server/config/config.js
@@ -2,18 +2,18 @@ const config = {
   production: {
     username: process.env.POSTGRES_USERNAME,
     password: process.env.POSTGRES_PASSWORD,
-    database: "mine_users",
-    host: "mine-support-postgres-users",
+    database: 'mine_users',
+    host: 'mine-support-postgres-users',
     port: 5432,
-    dialect: "postgres"
+    dialect: 'postgres'
   },
   development: {
     username: process.env.POSTGRES_USERNAME,
     password: process.env.POSTGRES_PASSWORD,
-    database: "mine_users",
-    host: "mine-support-postgres-users",
+    database: 'mine_users',
+    host: 'mine-support-postgres-users',
     port: 5432,
-    dialect: "postgres"
+    dialect: 'postgres'
   }
 }
 
