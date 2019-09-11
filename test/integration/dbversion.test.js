c46eef7540d5c92a37a29ae2cb25ac4ba9fefbfc
Matthew Collins <matthew.collins@defra.gov.uk>
2019-09-11 14:51:29 +0100

    Database version checking code added

--- test/integration/dbversion.test.js
+++ test/integration/dbversion.test.js
@@ -13,7 +13,7 @@ describe('User service integration test', () => {
     await truncateVersionTable()
   })
 
-  test('dbVersion gets a list of available migrations', async () => {
+  test('dbVersion gets a list of available migrations', () => {
     expect(dbVersion.availableVersions.length).toBeGreaterThan(0)
   })
 
@@ -25,5 +25,40 @@ describe('User service integration test', () => {
   test('dbVersion reports a list of pending migrations', async () => {
     const pending = await dbVersion.getPending()
     expect(pending.length).toBeGreaterThan(0)
+  })
+
+  test('dbVersion reports an issue if there are database version inconsistancies', async () => {
+    expect.assertions(3)
+    try {
+      const retval = await dbVersion.versionCorrect()
+      if (retval) {
+        console.log('Found no errors')
+      }
+    } catch (error) {
+      expect(error.message).toEqual('No database version could be found')
+    }
+    // Pretend we've got an existing migration in the database
+    await dbVersion.umzug.storage.model.upsert({ name: dbVersion.highestVersion })
+    try {
+      const retval = await dbVersion.versionCorrect()
+      if (retval) {
+        expect(retval).toEqual(true)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+    // Pretend we've got an unknown migration in the database
+    await dbVersion.umzug.storage.model.upsert({ name: 'zzzzzzzzzzesttest.nofile' })
+    try {
+      const retval = await dbVersion.versionCorrect()
+      if (retval) {
+        console.log('Found no errors')
+      }
+    } catch (error) {
+      expect(error.message).toEqual('Current database version unknown to this code')
+    }
+  })
+
+  afterAll(async () => {
   })
 })
