2836fe295256ea14b63565893243cf0f45dfadfe
Matthew Collins <matthew.collins@defra.gov.uk>
2019-09-12 13:30:07 +0100

    Tidy up exception testing in unit tests

--- test/integration/dbversion.test.js
+++ test/integration/dbversion.test.js
@@ -28,39 +28,18 @@ describe('User service integration test', () => {
 
   test('dbVersion reports an issue if there are no database versions stored', async () => {
     expect.assertions(1)
-    try {
-      const retval = await dbVersion.versionCorrect()
-      if (retval) {
-        console.log('Found no errors')
-      }
-    } catch (error) {
-      expect(error.message).toEqual('No database version could be found')
-    }
+    await expect(dbVersion.versionCorrect()).rejects.toThrow()
   })
 
   test('dbVersion does not report an issue if there is a valid database version', async () => {
     expect.assertions(1)
     await dbVersion.umzug.storage.model.upsert({ name: dbVersion.highestVersion })
-    try {
-      const retval = await dbVersion.versionCorrect()
-      if (retval) {
-        expect(retval).toEqual(true)
-      }
-    } catch (error) {
-      console.log(error)
-    }
+    await expect(dbVersion.versionCorrect()).resolves.toEqual(true)
   })
 
   test('dbVersion reports an issue if there are is an unknown database version', async () => {
     expect.assertions(1)
     await dbVersion.umzug.storage.model.upsert({ name: 'zzzzzzzzzzesttest.nofile' })
-    try {
-      const retval = await dbVersion.versionCorrect()
-      if (retval) {
-        console.log('Found no errors')
-      }
-    } catch (error) {
-      expect(error.message).toEqual('Current database version unknown to this code')
-    }
+    await expect(dbVersion.versionCorrect()).rejects.toThrow()
   })
 })
