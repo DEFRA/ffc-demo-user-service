ce63a377667c889f8ec495335a9213812103db0d
Matthew Collins <matthew.collins@defra.gov.uk>
2019-09-13 12:03:56 +0100

    Add check to health probe

--- server/routes/healthy.js
+++ server/routes/healthy.js
@@ -1,11 +1,22 @@
 const dbService = require('../services/database-service')
+const dbVersion = require('../dbversion')
 
 module.exports = {
   method: 'GET',
   path: '/healthy',
   options: {
     handler: async (request, h) => {
-      if (await dbService.isConnected()) {
+      let allOk = false
+      try {
+        if (await dbService.isConnected()) {
+          if (await dbVersion.versionCorrect()) {
+            allOk = true
+          }
+        }
+      } catch (error) {
+        allOk = false
+      }
+      if (allOk) {
         return h.response('ok').code(200)
       } else {
         return h.response('not ok').code(500)
